18e99fd813c1c78d0f4d142e199ea527651e69a2
cafouille01 <paulinevial13@gmail.com>
2013-02-12 13:41:56 +0100

    Changement des nom des commentaires et integration de la police Billabong pour bouton

--- HereIWas/src/com/example/hereiwas/HomeActivity.java
+++ HereIWas/src/com/example/hereiwas/HomeActivity.java
@@ -20,36 +20,35 @@ public class HomeActivity extends Activity {
         //setContentView(R.layout.activity_home);
         setContentView(R.layout.activity_signup);
        
-    // Changement de police pour le titre    
+    // --- BILLABONG --- Changement de police pour les button    
       
       // declaration        
-      /*  TextView textViewBillabongTitle = (TextView) findViewById(R.id.Title);
-        TextView textViewBillabongTitle2 = (TextView) findViewById(R.id.Title2);
+        TextView textViewBillabongOkButton = (TextView) findViewById(R.id.OkButton);
+        TextView textViewBillabongCancelButton = (TextView) findViewById(R.id.CancelButton);
         TextView textViewBillabongBtnLogin = (TextView) findViewById(R.id.LoginButton);
-        //TextView textViewBillabongBtnCancel = (TextView) findViewById(R.id.CancelButton);
+        TextView textViewBillabongBtnCancel = (TextView) findViewById(R.id.CancelButton);
         
       //initialisation
         Typeface fontBillabong = Typeface.createFromAsset(getAssets(), "Billabong.ttf");
-        textViewBillabongTitle.setTypeface(fontBillabong);
-        textViewBillabongTitle2.setTypeface(fontBillabong);
+        textViewBillabongOkButton.setTypeface(fontBillabong);
+        textViewBillabongCancelButton.setTypeface(fontBillabong);
         textViewBillabongBtnLogin.setTypeface(fontBillabong);
-        //textViewBillabongBtnCancel.setTypeface(fontBillabong);   */ 
+        textViewBillabongBtnCancel.setTypeface(fontBillabong);  
         
         
-   // Changement de la police des champs, texte
+   // --- ROBOTO BLACK-ITALIC ---- Changement de la police des champs, texte
       
         // declaration
         TextView textViewRobotoBILogin = (TextView) findViewById(R.id.champLogin);
         TextView textViewRobotoBIPassword = (TextView) findViewById(R.id.champPassword);
+        
         //initialisation
         Typeface fontRobotoBlackItalic = Typeface.createFromAsset(getAssets(), "Roboto-BlackItalic.ttf");
         textViewRobotoBIPassword.setTypeface(fontRobotoBlackItalic);
-        
-
         textViewRobotoBILogin.setTypeface(fontRobotoBlackItalic);
         
     
-    // Police pour les hint des editText 
+    // --- ROBOTO-LIGHT --- Police pour les hint des editText 
         // declaration
         TextView textViewRobotoLightLogin = (TextView) findViewById(R.id.champHintLogin);
         TextView textViewRobotoLightPassword = (TextView) findViewById(R.id.champHintPassword);
